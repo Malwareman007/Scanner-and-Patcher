c931b41d65b611711f8f5703ed979c24a6b184a0
Kushagra Ojha <86009160+Malwareman007@users.noreply.github.com>
2022-05-07 17:48:50 +0530

    Update xss.py

--- XSS Scanner/xss.py
+++ XSS Scanner/xss.py
@@ -5,4 +5,13 @@ from selenium.common.exceptions import TimeoutException
 import warnings, argparse, sys                                       
 
 warnings.filterwarnings('ignore')
-     
+    
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--disable-xss-auditor')
+options.add_argument('--disable-web-security')
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--no-sandbox')
+options.add_argument('--log-level=3')
+options.add_argument('--disable-notifications')
+driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", chrome_options=options)
