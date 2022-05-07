ecbe807569e0fa96b22cca2b91306f9ecbb319c4
Kushagra Ojha <86009160+Malwareman007@users.noreply.github.com>
2022-05-07 17:47:51 +0530

    Create xss.py

---
+++ XSS Scanner/xss.py
@@ -0,0 +1,8 @@
+from selenium import webdriver                                       
+from selenium.webdriver.support.ui import WebDriverWait              
+from selenium.webdriver.support import expected_conditions as EC     
+from selenium.common.exceptions import TimeoutException              
+import warnings, argparse, sys                                       
+
+warnings.filterwarnings('ignore')
+     
