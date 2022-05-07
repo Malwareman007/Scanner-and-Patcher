7ddf69943779c3c0874f9020987ac9afd4f4556e
Kushagra Ojha <86009160+Malwareman007@users.noreply.github.com>
2022-05-07 17:55:47 +0530

    Update xss.py

--- XSS Scanner/xss.py
+++ XSS Scanner/xss.py
@@ -4,8 +4,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException              
 import warnings, argparse, sys                                       
 
-warnings.filterwarnings('ignore')
-    
+warnings.filterwarnings('ignore')    
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 options.add_argument('--disable-xss-auditor')
@@ -15,3 +14,23 @@ options.add_argument('--no-sandbox')
 options.add_argument('--log-level=3')
 options.add_argument('--disable-notifications')
 driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", chrome_options=options)
+parser = argparse.ArgumentParser()
+parser.add_argument('-u', '--url', required=True)
+parser.add_argument('-w', '--wordlist', required=True)
+args = parser.parse_args()
+
+if not '{fuzz}' in args.url:
+    sys.exit("Need {fuzz} parameter !")
+else:
+    target = args.url
+wordlist = args.wordlist
+for payload in open(wordlist, "r").readlines():
+    url = target.replace('{fuzz}', payload)
+    driver.get(url)
+    try:
+        WebDriverWait(driver, 3).until(EC.alert_is_present())
+        alert = driver.switch_to_alert()
+        alert.accept()
+        print("XSS alert accepted ! with", payload)
+    except TimeoutException:
+        print("XSS doesn\'t work with", payload)
