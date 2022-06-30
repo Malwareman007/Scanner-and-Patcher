99f88049c8e17675102c07238567af1ef59b0055
Kushagra Ojha <86009160+Malwareman007@users.noreply.github.com>
2022-06-30 19:36:15 +0530

    commit
    Signed-off-by: Kushagra Ojha <86009160+Malwareman007@users.noreply.github.com>

--- setup/linux-setup.sh
+++ setup/linux-setup.sh
@@ -13,6 +13,21 @@ sudo apt-get upgrade -y
 # install required packages
 sudo apt-get install python3-pip software-properties-common build-essential cmake libgtk-3-dev libboost-all-dev -y
 
+# installing whatweb
+sudo apt-get install whatweb
+# installing host
+sudo apt-get install host
+# installing wget
+sudo apt-get install wget
+# installing uniscan
+sudo apt-get install uniscan
+# installing wafwoof
+pip install wafw00f
+# installing wapiti
+sudo apt-get install wapiti
+# installing uniscan
+apt-get install uniscan
+
 #installing golismero
 
 git clone https://github.com/golismero/golismero.git
