ebd6f353c27b323f041e99178dc13a7881c0afac
Warmonger <86009160+Malwareman007@users.noreply.github.com>
2022-08-08 22:17:45 +0530

    commit
    Signed-off-by: Warmonger <86009160+Malwareman007@users.noreply.github.com>

--- Rfi Scanner/Rfi.py
+++ Rfi Scanner/Rfi.py
@@ -6,7 +6,7 @@ elif not (fuzz)' in sys.argv[1]:
      sys.exit("Missing (fuzz) parameter")
 else:
      selected target sys.argv[1]
-url- "http://127.0.0.1/test.php
+url- "http://127.0.0.1/test.php"
 target=selected target.replace("(fuzz)", url) # replacing (fuzz)
 request=requests.get(target) #send request
 if b"114518875656586764489874" in request.content: # check if 11451875656586764489874 is in source code
