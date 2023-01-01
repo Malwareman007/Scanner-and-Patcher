0df811b775d2a86255b9a174bc5f67f19947eccc
Riya73 <72311878+Riya73@users.noreply.github.com>
2023-01-01 19:05:59 +0530

    Add files via upload
    Signed-off-by: Riya73 <72311878+Riya73@users.noreply.github.com>

---
+++ pages/Scanner&Patcher-frontend/index.js
@@ -0,0 +1,27 @@
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+// Dark Mode / light mode
+let darkmode = document.querySelector("#darkmode");
+
+darkmode.onclick = () => {
+  if (darkmode.classList.contains("bx-moon")) {
+    darkmode.classList.replace("bx-moon", "bx-sun");
+    document.body.classList.add("active");
+  } else {
+    darkmode.classList.replace("bx-sun", "bx-moon");
+    document.body.classList.remove("active");
+  }
+};
